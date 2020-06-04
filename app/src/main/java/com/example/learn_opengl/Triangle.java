6ca295a46a06a1b5f8c8c2c3e9bc830549028124
haigen.zhu <haigen.zhu@tcl.com>
2020-06-04 14:26:10 +0800

    can display square texture

--- app/src/main/java/com/example/learn_opengl/Triangle.java
+++ app/src/main/java/com/example/learn_opengl/Triangle.java
@@ -1,11 +1,14 @@
 package com.example.learn_opengl;
 
 import android.content.Context;
+import android.opengl.GLES10;
 import android.opengl.GLES20;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+
+
 
 public class Triangle implements Shape {
 
@@ -21,7 +24,8 @@ public class Triangle implements Shape {
                     "attribute vec2 a_TexCoord;\n"+
                     "varying vec2 v_TexCoord;\n"+
                     "void main() {" +
-                    "  v_TexCoord = a_TexCoord;" +
+                    "  v_TexCoord = vec2(a_TexCoord.x, 1. -a_TexCoord.y);" +
+                    "  v_color = a_color;" +
                     "  gl_Position = projectMatrix * vPosition;" +
                     "}";
 
@@ -33,6 +37,7 @@ public class Triangle implements Shape {
                     "uniform sampler2D u_TextureUnit;\n"+
                     "void main() {" +
                     "  gl_FragColor = texture2D(u_TextureUnit, v_TexCoord);" +
+                    //"  gl_FragColor = v_color;" +
                     "}";
 
 
@@ -43,11 +48,17 @@ public class Triangle implements Shape {
     private FloatBuffer colorBuffer;
 
     // number of coordinates per vertex in this array
-    static final int COORDS_PER_VERTEX = 3;
+    static final int COORDS_PER_VERTEX = 2;
+//    static float triangleCoords[] = {   // in counterclockwise order:
+//            1.0f, 1.0f, 0.0f, // top
+//            -1.0f, 1.0f, 0.0f, // bottom left
+//            -1.0f, -1.0f, 0.0f,  // bottom right
+//            1,-1,0
+//    };
+
     static float triangleCoords[] = {   // in counterclockwise order:
-            0.0f, 1.0f, 0.0f, // top
-            -1.0f, -1.0f, 0.0f, // bottom left
-            1.0f, -1.0f, 0.0f  // bottom right
+            //-1,-1,1,-1,-1,1,1,1
+            -1,1,-1,-1,1,-1,1,1
     };
 
     // Set color with red, green, blue and alpha (opacity) values
@@ -58,24 +69,37 @@ public class Triangle implements Shape {
     };
 
     float [] texture_pos={
-            0,1,0,0,1,0,1,1
+            0f, 1f, 0f, 0f, 1f, 0f, 1f, 1f
+            //0,0,1,0,0,1,1,1
     };
     public Triangle(Context context) {
+        mProgram = GLHelper.makeProgram(vertexShaderCode, fragmentShaderCode);
+
+
         vertexBuffer = GLHelper.createFloatBuffer(triangleCoords);
+        positionHandle = GLHelper.getAttr(mProgram, "vPosition");
+        GLES20.glVertexAttribPointer(positionHandle, COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT, false,
+                vertexStride, vertexBuffer);
+        GLES20.glEnableVertexAttribArray(positionHandle);
 
         colorBuffer = GLHelper.createFloatBuffer(color);
+        int colorHandle = GLHelper.getAttr(mProgram, "a_color");
+        GLES20.glEnableVertexAttribArray(colorHandle);
+        GLES20.glVertexAttribPointer(colorHandle, COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT, false,
+                4 * 4, colorBuffer);
 
         FloatBuffer texture_buffer = GLHelper.createFloatBuffer(texture_pos);
-        mProgram = GLHelper.makeProgram(vertexShaderCode, fragmentShaderCode);
         int aTexCoordLocation = GLHelper.getAttr(mProgram,"a_TexCoord");
         uTextureUnitLocation = GLHelper.getUniform(mProgram,"u_TextureUnit");
         GLES20.glVertexAttribPointer(aTexCoordLocation, 2, GLES20.GL_FLOAT, false, 2*4, texture_buffer);
         GLES20.glEnableVertexAttribArray(aTexCoordLocation);
         textureBean = GLHelper.loadTexture(context, R.drawable.pikachu);
-        // 开启纹理透明混合，这样才能绘制透明图片
-        GLES20.glEnable(GLES20.GL_BLEND);
-        GLES20.glBlendFunc(GLES20.GL_ONE, GLES20.GL_ONE_MINUS_SRC_ALPHA);
+//        // 开启纹理透明混合，这样才能绘制透明图片
     }
+
+
     GLHelper.TextureBean textureBean;
     int uTextureUnitLocation;
     int projectMatrixIndex;
@@ -83,22 +107,11 @@ public class Triangle implements Shape {
     @Override
     public void draw() {
 
-        // get handle to vertex shader's vPosition member
-        positionHandle = GLHelper.getAttr(mProgram, "vPosition");
-
-        // Enable a handle to the triangle vertices
-        GLES20.glEnableVertexAttribArray(positionHandle);
-
-        // Prepare the triangle coordinate data
-        GLES20.glVertexAttribPointer(positionHandle, COORDS_PER_VERTEX,
-                GLES20.GL_FLOAT, false,
-                vertexStride, vertexBuffer);
-
-        int colorHandle = GLHelper.getAttr(mProgram, "a_color");
-        GLES20.glEnableVertexAttribArray(colorHandle);
-        GLES20.glVertexAttribPointer(colorHandle, COORDS_PER_VERTEX,
-                GLES20.GL_FLOAT, false,
-                4 * 4, colorBuffer);
+//        int colorHandle = GLHelper.getAttr(mProgram, "a_color");
+//        GLES20.glEnableVertexAttribArray(colorHandle);
+//        GLES20.glVertexAttribPointer(colorHandle, COORDS_PER_VERTEX,
+//                GLES20.GL_FLOAT, false,
+//                4 * 4, colorBuffer);
 
         projectMatrixIndex = GLHelper.getAttr(mProgram, "projectMatrix");
 
@@ -107,7 +120,7 @@ public class Triangle implements Shape {
         GLES20.glUniform1i(uTextureUnitLocation, 0);
 
         // Draw the triangle
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, vertexCount);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, vertexCount);
     }
 
     @Override
